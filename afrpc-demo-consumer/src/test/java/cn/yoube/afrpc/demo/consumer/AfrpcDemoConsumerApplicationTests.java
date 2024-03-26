1fc5185db04c5c17271f7570b64c638c06f11ce0
LinMF <bmwz508@163.com>
2024-03-26 23:34:28 +0800

    refactor maven pom

--- afrpc-demo-consumer/src/test/java/cn/yoube/afrpc/demo/consumer/AfrpcDemoConsumerApplicationTests.java
+++ afrpc-demo-consumer/src/test/java/cn/yoube/afrpc/demo/consumer/AfrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,30 @@
 package cn.yoube.afrpc.demo.consumer;
 
+import cn.yoube.afrpc.demo.provider.AfrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class AfrpcDemoConsumerApplicationTests {
 
+    static ApplicationContext context;
+
+    @BeforeAll
+    static void init() {
+        context = SpringApplication.run(AfrpcDemoProviderApplication.class, "--server.port=8084");
+    }
+
     @Test
     void contextLoads() {
+    }
+
+    @AfterAll
+    static void destory() {
+        SpringApplication.exit(context, () -> 1);
     }
 
 }
