16918264c0423309a1a7061337bf0aa8c4cb0966
LinMF <bmwz508@163.com>
2024-03-27 20:14:25 +0800

    fix: unit test

---
+++ afrpc-demo-consumer/src/test/java/cn/yoube/afrpc/demo/provider/UserServiceImpl.java
@@ -0,0 +1,100 @@
+package cn.yoube.afrpc.demo.provider;
+
+import cn.yoube.afrpc.core.annotation.RpcProvider;
+import cn.yoube.afrpc.demo.api.User;
+import cn.yoube.afrpc.demo.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author LimMF
+ * @since 2024/3/7
+ **/
+@Component
+@RpcProvider
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    Environment environment;
+
+    @Override
+    public User findById(int id) {
+        return new User(id, "AF-" + environment.getProperty("server.port") +
+                "_" + System.currentTimeMillis());
+    }
+
+    @Override
+    public User findById(int id, String name) {
+        return new User(id, "AF-" + name + "_" + System.currentTimeMillis());
+    }
+
+    @Override
+    public long getId(long id) {
+        return id;
+    }
+
+    @Override
+    public long getId(User user) {
+        return user.getId().longValue();
+    }
+
+    @Override
+    public long getId(float id) {
+        return 1L;
+    }
+
+    @Override
+    public String getName() {
+        return "AF123";
+    }
+
+    @Override
+    public String getName(int id) {
+        return "AF-" + id;
+    }
+
+    @Override
+    public int[] getIds() {
+        return new int[] {100,200,300};
+    }
+
+    @Override
+    public long[] getLongIds() {
+        return new long[]{1,2,3};
+    }
+
+    @Override
+    public int[] getIds(int[] ids) {
+        return ids;
+    }
+
+    @Override
+    public Long[] getLongIds(Long[] longIds) {
+        return longIds;
+    }
+
+    @Override
+    public List<User> getList(List<User> userList) {
+        return userList;
+    }
+
+    @Override
+    public Set<User> getSet(Set<User> userSet) {
+        return userSet;
+    }
+
+    @Override
+    public Map<String, User> getMap(Map<String, User> userMap) {
+        return userMap;
+    }
+
+    @Override
+    public Boolean getFlag(boolean flag) {
+        return flag;
+    }
+}
